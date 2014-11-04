cd0a38658d39766ca125bbf0cd814e268af5115e
Galen Gidman <galen@galengidman.com>
2014-11-04 14:52:58 -0600

    added functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions
+ */
+
+/**
+ * Include Static Templates plugin.
+ */
+require_once get_template_directory() . '/library/static-templates/static-templates.php';
+
+/**
+ * Include assets manager.
+ */
+require_once get_template_directory() . '/includes/assets.php';
