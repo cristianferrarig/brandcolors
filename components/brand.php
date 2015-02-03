f145bb14a7a49e16b4017e231f8ba456d5948beb
Galen Gidman <galen@galengidman.com>
2015-02-03 12:44:11 -0600

    fixed old phpColors code

--- components/brand.php
+++ components/brand.php
@@ -1,7 +1,5 @@
 <?php
 
-use Mexitek\PHPColors\Color;
-
 $colors = bcs_get_colors();
 $color_count = count( $colors );
 
@@ -14,48 +12,10 @@ $color_count = count( $colors );
   </header>
 
   <div class="brand-colors cf">
-    <?php
-
-    foreach ( $colors as $color ) :
-
-      $color     = new Color( $color );
-
-      $shade     = ( $color->isDark( $color->darken( 30 ) ) ? 'dark' : 'light' );
-
-      $hex       = $color->getHex();
-      $rgb       = '';
-      $hsl       = '';
-
-      $i         = 1;
-      $rgb_array = $color->getRgb();
-      foreach ( $rgb_array as $val ) {
-        $rgb .= $val;
-
-        if ( $i != 3 ) $rgb .= ', ';
-
-        $i++;
-      }
-
-      $i         = 1;
-      $hsl_array = $color->getHsl();
-      foreach ( $hsl_array as $val ) {
-        $hsl .= $val;
-
-        if ( $i == 1 ) {
-          $hsl .= '°';
-        } else {
-          $hsl .= '%';
-        }
-
-        if ( $i != 3 ) $hsl .= ' ';
-
-        $i++;
-      }
-
-    ?>
+    <?php foreach ( $colors as $color ) : ?>
       <div class="color" style="width: <?php echo 100 / $color_count; ?>%;">
-        <div class="color-inner <?php echo $shade; ?>" style="background-color: #<?php echo $hex; ?>" data-color-hex="<?php echo $hex; ?>" data-color-rgb="<?php echo $rgb; ?>" data-color-hsl="<?php echo $hsl; ?>">
-          <input type="text" class="color-code" size="6" value="<?php echo $hex; ?>" readonly>
+        <div class="color-inner" style="background-color: #<?php echo $color; ?>" data-color-hex="<?php echo $color; ?>">
+          <input type="text" class="color-code" size="6" value="<?php echo $color; ?>" readonly>
         </div>
       </div>
     <?php endforeach; ?>
