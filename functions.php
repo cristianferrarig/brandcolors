f9ac700c983adaf52b1842a820a7328fd6ae5e5a
Galen Gidman <galen@galengidman.com>
2015-02-04 12:37:00 -0600

    removed bower

--- functions.php
+++ functions.php
@@ -14,11 +14,21 @@ add_filter( 'show_admin_bar', '__return_false' );
 function bc_assets() {
 
   // Styles
-  wp_enqueue_style( 'genericons',   get_template_directory_uri() . '/bower_components/Genericons/genericons.css' );
-  wp_enqueue_style( 'brandcolors',  get_template_directory_uri() . '/assets/css/brandcolors.css' );
+  wp_enqueue_style( 'genericons',  get_template_directory_uri() . '/bower_components/Genericons/genericons.css' );
+  wp_enqueue_style( 'brandcolors', get_template_directory_uri() . '/assets/css/brandcolors.css' );
 
   // Scripts
-  wp_enqueue_script( 'brandcolors',   get_template_directory_uri() . '/assets/js/brandcolors.js', array( 'jquery' ) );
+  wp_enqueue_script( 'brandcolors', get_template_directory_uri() . '/assets/js/brandcolors.js', array( 'jquery' ) );
 
 }
 add_action( 'wp_enqueue_scripts', 'bc_assets' );
+
+/**
+ * Add theme support for various WP features.
+ */
+function bc_support() {
+
+  add_theme_support( 'title-tag' );
+
+}
+add_action( 'after_setup_theme', 'bc_support' );
--- modules/access.php
+++ modules/access.php
@@ -2,7 +2,7 @@
   <div class="current-collection"><span id="collection-count">0 brands</span> in collection <span id="share-collection" class="share-collection is-hidden"><i class="genericon genericon-link"></i></span></div>
 
   <div class="download">
-    <a href="#" class="btn"><i class="btn-icon genericon genericon-download"></i>Collection</a>
-    <a href="#" class="btn"><i class="btn-icon genericon genericon-download"></i>All</a>
+    <a href="#" class="btn"><i class="btn-icon genericon genericon-cloud-download"></i>Collection</a>
+    <a href="#" class="btn"><i class="btn-icon genericon genericon-cloud-download"></i>All</a>
   </div>
 </div>
